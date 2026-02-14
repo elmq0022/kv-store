5cfbcc8b3407d7a05bb9de143147ae0a1cddc76a
Aaron Elmquist <aaron.elmquist@protonmail.com>
2026-02-14 10:56:34 -0600

    adds simple and bulk string parsing

--- parser/parser.go
+++ parser/parser.go
@@ -2,7 +2,17 @@ package parser
 
 import (
 	"bufio"
+	"errors"
 	"io"
+	"strconv"
+)
+
+const (
+	typeBulkString   byte = '$'
+	typeArray        byte = '*'
+	typeSimpleString byte = '+'
+	typeInteger      byte = ':'
+	typeError        byte = '-'
 )
 
 type Value struct {
@@ -13,6 +23,89 @@ type Value struct {
 
 func Parse(r io.Reader) (Value, error) {
 	br := bufio.NewReader(r)
-	_ = br
-	return Value{}, nil
+	var err error
+
+	bytecode, err := br.ReadByte()
+	if err != nil {
+		return Value{}, err
+	}
+
+	val := Value{}
+	val.Type = bytecode
+
+	switch bytecode {
+	case typeBulkString:
+		val.Bytes, err = parseBulkString(br)
+	case typeSimpleString:
+		val.Bytes, err = parseSimpleString(br)
+	case typeInteger:
+		val.Bytes, err = parseInteger(br)
+	case typeError:
+		val.Bytes, err = parseError(br)
+	case typeArray:
+		val.Array, err = parseArray(br)
+	default:
+		return Value{}, errors.New("bytecode not implemented")
+	}
+
+	return val, err
+}
+
+func parseBulkString(br *bufio.Reader) ([]byte, error) {
+	b, err := br.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+	if len(b) < 2 {
+		return nil, errors.New("bad parse")
+	}
+
+	b = b[:len(b)-2]
+	nWant, err := strconv.Atoi(string(b))
+	if err != nil {
+		return nil, err
+	}
+
+	if nWant == -1 {
+		return nil, nil
+	}
+
+	p := make([]byte, nWant)
+	_, err = io.ReadFull(br, p)
+	if err != nil {
+		return nil, err
+	}
+	code, err := br.ReadByte()
+	if code != '\r' || err != nil {
+		return nil, errors.New("nope")
+	}
+	code, err = br.ReadByte()
+	if code != '\n' || err != nil {
+		return nil, errors.New("nope")
+	}
+
+	return p, nil
+}
+
+func parseSimpleString(br *bufio.Reader) ([]byte, error) {
+	b, err := br.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+	if len(b) < 2 {
+		return nil, errors.New("bad parse")
+	}
+	return b[:len(b)-2], nil
+}
+
+func parseInteger(br *bufio.Reader) ([]byte, error) {
+	return nil, nil
+}
+
+func parseError(br *bufio.Reader) ([]byte, error) {
+	return nil, nil
+}
+
+func parseArray(br *bufio.Reader) ([]Value, error) {
+	return nil, nil
 }
